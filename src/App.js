40afa976da8e5ee1b2c94a0e79b23d9b1a6c2380
KalimGithub <sk22kalim@gmail.com>
2023-10-15 00:30:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+import Footer from "./Components/Footer"
+import Header from "./Components/Header"
+import Timer from "./Components/Timer"
+import TextArea from "./Components/TextArea"
+const App = () => {
+    return(
+        <div>
+            <Header />
+            <Timer />
+            <Footer />
+            <TextArea />
+        </div>
+    )
+}
+
+export default App
---
+++ src/Components/TextArea.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TextArea = () => {
+    return(
+        <div className="text-area">
+            <textarea rows={12} cols={70} id="text-area-input">
+            sample text goes here.....</textarea>
+        </div>
+    )
+}
+export default TextArea
